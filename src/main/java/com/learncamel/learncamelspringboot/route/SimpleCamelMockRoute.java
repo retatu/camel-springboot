6882481bc17d9f30845430af035b833f4d0a5286
retatu <mrehbein45@gmail.com>
2018-05-25 10:47:47 -0300

    Criado log com lombok

--- src/main/java/com/learncamel/learncamelspringboot/route/SimpleCamelMockRoute.java
+++ src/main/java/com/learncamel/learncamelspringboot/route/SimpleCamelMockRoute.java
@@ -1,17 +1,22 @@
 package com.learncamel.learncamelspringboot.route;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 public class SimpleCamelMockRoute extends RouteBuilder {
 
     @Autowired
     Environment environment;
     @Override
     public void configure() throws Exception {
+
+        log.info("Starting the route....");
+
         from("{{startRoute}}")
             .log("Timer Invoked and body is "+ environment.getProperty("message"))
                 .choice()
@@ -21,5 +26,7 @@ public class SimpleCamelMockRoute extends RouteBuilder {
                     .otherwise()
                         .log("Is a mock").end()
                 .to("{{toRoute1}}");
+
+        log.info("Ending the route....");
     }
 }
